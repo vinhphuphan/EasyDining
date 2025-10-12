f45a2b1d12bec9751fc903036030cc4346943f11
vinhphuphan <109563372+vinhphuphan@users.noreply.github.com>
2025-10-12 12:20:00 +1000

    Building AuthContext and setup api call for testing api

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -8,7 +8,9 @@ import "./globals.css"
 import Header from "@/components/header/Header"
 import { CreateOrderModalProvider } from "@/context/CreateOrderModalProvider"
 import CreateOrderModalRenderer from "@/components/modals/create-order-modal-renderer"
-import { Toaster } from "sonner";
+import { Toaster } from "sonner"
+import { ErrorBoundary } from "@/components/error-boundary"
+import { AuthProvider } from "@/context/AuthContext";
 
 export const metadata: Metadata = {
   title: "EasyDining - Restaurant POS System",
@@ -27,11 +29,15 @@ export default function RootLayout({
   return (
     <html lang="en" className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
       <body>
-        <CreateOrderModalProvider>
-          <Header />
-          <Suspense fallback={null}>{children}</Suspense>
-          <CreateOrderModalRenderer />
-        </CreateOrderModalProvider>
+        <ErrorBoundary>
+          <AuthProvider>
+            <CreateOrderModalProvider>
+              <Header />
+              <Suspense fallback={null}>{children}</Suspense>
+              <CreateOrderModalRenderer />
+            </CreateOrderModalProvider>
+          </AuthProvider>
+        </ErrorBoundary>
         <Toaster
           theme="system"
           richColors
